1a6762c402ddebb6736ccf0ade74b82d7719a3e0
David Valentine <DValentine@ethosgroup.com>
2024-11-27 14:42:33 -0600

    Intersection observer POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,36 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng19';
+  @ViewChildren('elementBox') elementBoxes: QueryList<ElementRef> | undefined;
+  elementStrings:string[] = []
+  ngOnInit() {
+    for (let i = 0; i < 50; i++) {
+      this.elementStrings.push(`Entry ${i}`);
+    }
+  }
+
+  ngAfterViewInit() {
+    const options = {
+      rootMargin: "-200px 0px -200px 0px",
+      threshold: 1,
+    };
+
+    const callback:IntersectionObserverCallback = (entries, observer) => {
+
+      if(entries[entries.length-10].isIntersecting){
+        for (let i = 0; i < 50; i++) {
+          this.elementStrings.push(`Entry ${this.elementStrings.length+1}`);
+        }
+      }
+      entries.forEach(entry => {
+        entry.target.classList.toggle('is-visible', entry.isIntersecting);
+        observer.unobserve(entry.target);
+      });
+      this.elementBoxes!.forEach(element => observer.observe(element.nativeElement));
+    };
+    const observer = new IntersectionObserver(callback, options);
+    this.elementBoxes!.forEach(element => observer.observe(element.nativeElement));
+  }
 }
